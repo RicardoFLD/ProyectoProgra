dd196472c6e2d36653d785d38bdfaa6525f1fc19
adriandelgm <99231001+adriandelgm@users.noreply.github.com>
2022-04-20 21:58:04 -0600

    Update geneva.py

--- geneva.py
+++ geneva.py
@@ -106,7 +106,134 @@ def genevaControl():
     cal=Calendar(control, selectmode="day", width=35, height=10)
     cal.config(headersbackground='#b0b0b0', foreground='#000', background='#ededed', headersforeground ='#ededed')
     cal.place(x="187", y="78")
-
+    
+    #TIMER
+    
+    class pomodoroTimer:
+        self.root=TK()
+        self.root.geometry("600x300")
+        self.root-title("Temporizador de Pomodoro")
+        
+        self.s=ttk.Style()
+        self.s.configure("TNotebook.tab", font=("Arial", 16))
+        self.s.configure("TButton.tab", font=("Arial", 16))
+        
+        self.tabs=ttk.Notebook(self.root)
+        self.tabs.pack(fill="both", pady=10, expand=True)
+        
+        self.tab1=ttk.Frame(self.tabs, width=600, height=100)
+        self.tab2=ttk.Frame(self.tabs, width=600, height=100)
+        self.tab3=ttk.Frame(self.tabs, width=600, height=100)
+        
+        self.pomodoro_timer_label=ttk.Label(self.tab1, text="25:00", font=("Arial", 43))
+        self.pomodor_timer_label.pack(pady=20)
+        
+        self.short_break_timer_label=ttk.Label(self.tab2, text="05:00", font=("Arial", 43))
+        self.short_break_timer_label.pack(pady=20)
+        
+        self.long_break_timer_label=ttk.Label(self.tab3, text="15:00", font=("Arial", 43))
+        self.long_break_timer_label.pack(pady=20)
+        
+        self.tabs.add(self.tab1, text="Pomodoro")
+        self.tabs.add(self.tab2, text="Receso corto")
+        self.tabs.add(self.tab3, text="Receso largo")
+        
+        self.grid_layout=ttk.Frame(self.root)
+        self.grid_layout.pack(pady=18)
+        
+        self.start_button=ttk.Button(self.grid_layout, text="Iniciar", command=self.start_timer_thread)
+        self.start_button.grid(row=0, column=0)
+        
+        self.skip_button=ttk.Button(self.grid_layout, text="Saltar", command=self.skip_clock)
+        self.skip_button.grid(row=0, column=2)
+        
+        self.reset_button=ttk.Button(self.grid_layout, text="Reiniciar", command=self.reset_clock)
+        self.reset_button.grid(row=0, column=3)
+        
+        self.pomodoro_counter_label=ttk.Label(self.grid_layout, text="Pomodoros: 0", font=("Arial", 16))
+        self.pomodoro_counter_label.grid(row=1, column=0, columnspan=3, pady=10)
+        
+        self.pomodoros=0
+        self.skipped=False
+        self.stopped=False
+        self.running=False
+        
+        self.root.mainloop()
+        
+        def start_timer_thread(self):
+            if not self.running:
+                t=threading.thread(target=self.start_timer)
+                t.start()
+                self.running=True
+            
+        def start_timer(self):
+            self.stopped=False
+            self.skipped=False
+            timer_id=self.tabs.index(self.tabs.select())+1
+            
+            if timer_id==1:
+                full_seconds=60*25
+                while full_seconds>0 and not self.stopped:
+                    minutes, seconds=divmod(full_seconds,60)
+                    self.pomodoro_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                    self.root.update()
+                    time.sleep(1)
+                    full_seconds-=1
+                if not self.stopped or self.skipped:
+                    self.pomodoros+=1
+                    self.pomodoro_counter_label.config(text=f"Pomodoros:{self.pomodoros}")
+                    if self.pomodoros % 4==0:
+                        self.tabs.select(2)
+                        self.start_timer()
+                    else:
+                        self.tabs.select(1)
+                    self.start_timer()
+            elif timer_id ==2:
+                full_seconds=60*5
+                while full_seconds>0 and not self.stopped:
+                    minutes, seconds=divmod(full_seconds,60)
+                    self.short_break_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                    self.root.update()
+                    time.sleep(1)
+                    full_seconds-=1
+                if not self.stopped or self.skipped:
+                    self.tabs.select(0)
+                    self.start_timer()
+            elif timer_id==3:
+                full_seconds=60*15
+                while full_seconds>0 and not self.stopped:
+                    minutes, seconds=divmod(full_seconds,60)
+                    self.long_break_timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
+                    self.root.update()
+                    time.sleep(1)
+                    full_seconds-=1
+                if not self.stopped or self.skipped:
+                    self.tabs.select(0)
+                    self.start_timer()
+                    
+        def reset_clock(self):
+            self.stopped=True
+            self.skipped=False
+            self.pomodoros=0
+            self.pomodoro_timer_label.config(text="25:00")
+            self.short_break_timer_label.config(text="05:00")
+            self.long_break_timer_label.config(text="15:00")
+            self.pomodoro_counter_label.config(text="Pomodoros: 0")
+            self.running=False
+            
+        def skip_clock(self):
+            current_tab=self.tabs.index(self.tabs.select())
+            if current_tab==0:
+                 self.pomodoro_timer_label.config(text="25:00")
+            elif current_tab==1:
+                 self.short_break_timer_label.config(text="05:00")
+            elif current_tab==2:
+                 self.long_break_timer_label.config(text="15:00")
+            self.stopped=True
+            self.skipped=True
+            
+    Button(control, text="Pomodoro", font="Arial 10", width=10, command=pomodoroTimer()).place(x="31", y="123")
+    
     #TO DO LIST
     
     Label(control, text="Lista de tareas", font="Arial 10").place(x="510", y="51")
